a67c1d35f5e9f3b716601c34653a81abf1156260
“zxf” <15924194882@139.com>
2021-07-02 14:49:02 +0800

    first commit

---
+++ src/test/java/testcases/LoginTest.java
@@ -0,0 +1,10 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+    @Test
+    public void LoginSuccess(){
+        System.out.println("登录成功");
+    }
+}
---
+++ src/test/java/testcases/RegisterTest.java
@@ -0,0 +1,10 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class RegisterTest {
+    @Test
+    public void RegisterSuccess(){
+        System.out.println("注册成功");
+    }
+}
